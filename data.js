570a7622b38f1940d4f5dc37b65944ced9fd89dd
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-08-29 16:29:30 +0200

    Refactor parsing

--- data.js
+++ data.js
@@ -41,3 +41,79 @@ class PageTree {
 
   
 }
+
+class Page {
+  
+  /**
+   * Creates a new Page instance.
+   * @param {string} id - The unique identifier of the page.
+   * @param {string} name - The name of the page.
+   */
+  constructor(id, name) {
+	
+	/**
+     * The unique identifier of the page.
+     * @type {string}
+     */
+    this.id = id;
+
+	/**
+	 * Display name of the page.
+	 * @type {string}
+	 */
+	this.name = name;
+
+	/**
+	 * Links that point outiside of this page
+	 * @type {Set<Page>}
+	 */
+	this.links = new Set();    
+  }
+
+  /**
+   * Adds a link from this page to another page.
+   * @param {Page} page - The page to link to.
+   */
+  addLink(page) {
+	this.links.add(page);
+  }
+}
+
+class PageGraph {
+
+  /**
+   * Creates a PageGraph instance
+   */
+  constructor() {
+
+	/**
+     * A map of pages, keyed by their ID.
+     * @type {Map<string, Page>}
+     */
+    this.pages = new Map();    
+  }
+
+  /**
+   * Adds a new page to the graph 
+   */
+  addPage(id, name) {
+	if (this.pages.has(id)) {
+	  throw new Error(`Page with ID ${id} already exists.`);
+	}
+
+	const page = new Page(id, name);
+	this.pages.set(id, page);
+  }
+
+  addLink(sourceId, targetId) {
+	const sourcePage = this.pages.get(sourceId);
+	const targetPage = this.pages.get(targetId);
+
+	if (!sourcePage || !targetPage) {
+	  throw new Error("Both pages must exist to create a link.");
+	}
+
+	sourcePage.addLink(targetPage);
+  }
+
+}
